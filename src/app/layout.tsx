6ff222e9d5fa2b946931f37091430ececd711e8a
walker2398 <13916981947@163.com>
2025-02-07 23:51:01 +0800

    <fix>:grammar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import "./globals.css";
+import Link from 'next/link'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,11 +32,11 @@ export default function RootLayout({
         <div className="w-screen fixed">
           <div className="lg:px-8 lg:py-3">
             <div className="flex items-center">
-              <a href="/" className="justify-self-start">Luna</a>
+              <Link href="/" className="justify-self-start">Luna</Link>
               <div className="lg:ml-auto">
                 <div className="flex space-x-8">
                   <a>Download</a>
-                  <a href="/console">控制台</a>
+                  <Link href="/console">控制台</Link>
                   <a>Login</a>
                 </div>
                 </div>
